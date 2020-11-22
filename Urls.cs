cc4baa645426b932c6c37dd59bf4c208fc1b38c0
Hronos <20587929+Hronos2t@users.noreply.github.com>
2020-11-22 21:38:40 +0300

    Update Urls.cs
    update 7z to 1900

--- Urls.cs
+++ Urls.cs
@@ -4,7 +4,7 @@
     {
         public static string LastVersionInfo = "http://rubotaion.ru/curver.xml";
         public static string Rubot = "http://rubotaion.ru/content/files/rubot.7z";
-        public static string SevenZip = "http://7-zip.org/a/7z1805.exe";
+        public static string SevenZip = "http://7-zip.org/a/7z1900.exe";
         public static string RubotGithub = "https://github.com/Hronos2t/rubot-binary";
     }
 }
